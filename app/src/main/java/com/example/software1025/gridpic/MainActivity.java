770cf7090bcc89df4d04a9090b781f07f6847aa0
goonhunter <32113094+goonhunter@users.noreply.github.com>
2017-10-22 21:39:05 -0600

    Initial Add

---
+++ app/src/main/java/com/example/software1025/gridpic/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.software1025.gridpic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
